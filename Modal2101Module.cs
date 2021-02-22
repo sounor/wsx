75a22df6a2f445389e3dd447b8c58a0fb2e1fad0
sounor <wqlishe@gmail.com>
2021-02-22 14:37:31 +0800

    添加项目文件。

---
+++ Modal2101Module.cs
@@ -0,0 +1,20 @@
+﻿using Modal2101.Modal2101.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Modal2101.Modal2101
+{
+    public class Modal2101Module : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
